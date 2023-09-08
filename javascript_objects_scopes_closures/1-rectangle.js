7ffb0b5a1eb425ef10881f348a0f8a603dfcd971
ogutuderick <ogutuderick4@gmail.com>
2023-09-08 19:03:05 +0300

    Rectangle #1

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+class Rectangle {
+    constructor(w, h) {
+        /*initializing: 
+        the attribute width=w
+        the attribute height=h
+        */
+        this.width = w;
+        this.height = h;
+    }
+}
+module.exports = Rectangle;
