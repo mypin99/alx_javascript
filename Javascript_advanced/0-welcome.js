da21607248f5e3c735adc09eb2e3568868cf89f1
ogutuderick <ogutuderick4@gmail.com>
2023-09-16 00:16:11 +0300

    Lexical scoping and welcome message

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+function welcome(firstName, lastName) {
+    function displayFullName() {
+      let fullName = firstName + ' ' + lastName;
+      alert('Welcome ' + fullName + ' !');
+    }
+    displayFullName();
+  }
+  
